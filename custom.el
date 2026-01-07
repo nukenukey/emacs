eb7445112ce5503d4953d9c1729a05bc249384f1
august <august@slate>
2026-01-07 02:47:55 -0600

    added custom.el and moved custom stuff there

---
+++ custom.el
@@ -0,0 +1,20 @@
+;; -*- lexical-binding: t; -*-
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons company counsel dashboard doom-modeline fireplace
+				   fish-mode ivy-rich lsp-mode magit multiple-cursors
+				   org-bullets php-mode pink-bliss-uwu-theme rust-mode
+				   tldr typescript-mode vterm)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 145 :width normal :foundry "JB" :family "JetBrains Mono"))))
+ '(cursor ((t (:background "sienna")))))
--- init.el
+++ init.el
@@ -2,16 +2,8 @@
 
 ;; Copyright (C) 2025 aug <baleofhay@proton.me>
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(all-the-icons company counsel dashboard doom-modeline fireplace
-				   fish-mode ivy-rich lsp-mode magit multiple-cursors
-				   org-bullets php-mode pink-bliss-uwu-theme rust-mode
-				   tldr typescript-mode vterm)))
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load-file custom-file)
 
 (use-package use-package
   :init
@@ -268,11 +260,3 @@
   (keymap-global-set "C-x M-C" '(lambda ()
                                   (interactive)
                                   (save-some-buffers)
-                                  (restart-emacs))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 145 :width normal :foundry "JB" :family "JetBrains Mono"))))
- '(cursor ((t (:background "sienna")))))

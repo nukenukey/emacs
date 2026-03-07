d8f7eaf0c6788e73e36d401c311a646ae23c5694
nukenukey <micklebubble22@gmail.com>
2026-03-06 23:47:13 -0600

    fixed up start_client.sh

--- start_client.sh
+++ start_client.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 if [ "$XDG_CURRENT_DESKTOP" != "sway" ] ; then
 	SELECT_FRAME="(toggle-frame-fullscreen) (select-frame-set-input-focus (selected-frame))"
 else
@@ -6,17 +6,17 @@ else
 fi
 
 start_daemon () {
-		emacs --daemon || { notify-send "emacs daemon could not be started" ; exit 1 }
+		emacs --daemon || { notify-send "emacs daemon could not be started" ; return 1 ; }
 }
 
 restart_emacs () {
 		echo "restarting emacs"
-		pkill -f "emacs --daemon" || { notify-send "emacs could not be restarted" ; exit 1 }
+		pkill -f "emacs --daemon" || { notify-send "emacs could not be restarted" ; return 1 ; }
 }
 
 start_client () {
-		emacsclient -c -a '' -e "(progn ${SELECT_FRAME} (dired-jump)))" || { notify-send "emacsclient could not be started" ; exit 1}
+		emacsclient -c -a '' -e "(progn ${SELECT_FRAME} (dired-jump)))" || { notify-send "emacsclient could not be started" ; return 1 ; }
 		# emacsclient -c -a '' -e "(progn ${SELECT_FRAME} (or (and (boundp 'conv/last-buffer) (conv/switch-to-last-buffer)) (dired-jump)))"
 }
 
-start_client || { restart_emacs && start_client }
+start_client || { restart_emacs && start_client ; }
